e5697643cf61692fd1d98b504a66253c9e6bb775
Michelle Caner <michellecaner@gmail.com>
2023-10-02 11:50:50 -0500

    books 4 & 5

--- Practice/05_04/script.js
+++ Practice/05_04/script.js
@@ -2,3 +2,15 @@
  * Note: This file is intentionally empty.
  * You can use it to test your skills at traversing the DOM using JavaScript.
  */
+document
+  .querySelectorAll("main p")
+  .forEach((item) => (item.style.backgroundColor = "blue"));
+
+document.querySelector(".backpack__name").append(" BOOGIE");
+
+// let newImg = document.querySelectorAll(".backpack__image");
+
+// newImg.src =
+//   "https://img.freepik.com/free-vector/green-frog-white-background_1308-110146.jpg?w=2000";
+
+
--- Practice/05_08/script.js
+++ Practice/05_08/script.js
@@ -3,3 +3,19 @@
  * You can use it to test your skills at adding, removing, or modifying
  * attributes, classes, and inline styles on elements.
  */
+document.querySelector(".site-title").classList.add("another-class");
+
+document.querySelector(".site-title").classList.remove("site-title");
+
+document
+  .querySelector("#pack01 .backpack__image img")
+  .setAttribute("alt", "Image of everyday backpack.");
+
+document.querySelector(".backpack__name").style.color = "red";
+
+document
+  .querySelector("#pack02 .backpack__image img")
+  .setAttribute(
+    "src",
+    "https://i.ebayimg.com/images/g/lBUAAOSwlMxhwLV7/s-l500.jpg"
+  );
